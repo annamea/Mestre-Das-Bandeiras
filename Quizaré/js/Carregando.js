0bd315c94c5ce4591a980320384e36dd928a68fc
Anna <anna.evangelista@aluno.senai.br>
2025-05-06 16:44:19 -0300

    Delete Quizaré directory

--- Quizaré/js/Carregando.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Tempo de espera antes de redirecionar (em milissegundos)
-    const tempoDeEspera = 3000; // 3 segundos
-
-    setTimeout(() => {
-        window.location.href = "Podio.html";
-    }, tempoDeEspera);
-});
--- Quizaré/js/loguin.js
+++
@@ -1,9 +0,0 @@
-function salvarNome() {
-    const nome = document.getElementById("username").value;
-    if (nome) {
-        localStorage.setItem("nomeUsuario", nome); 
-        window.location.href = "avatar.html"; 
-    } else {
-        alert("Por favor, digite um nome!");
-    }
-}
